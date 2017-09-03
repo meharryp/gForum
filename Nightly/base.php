56feabfa7a1a3a9d9bc6d8b31fddfb0eb0deab0a
J0sh0nat0r <jojo.parillon@gmail.com>
2017-09-03 22:56:56 +0100

    Update base.php
    Corrected missing $

--- Nightly/base.php
+++ Nightly/base.php
@@ -5,7 +5,7 @@ require 'config.php';
 $mysqli = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db, isset($mysql_port) ? $mysql_port : 3306);
 
 class gForum{
-	public static function post(post_text){
+	public static function post($post_text){
 		//Stuff
 	}
 
